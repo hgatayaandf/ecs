449de126a930c04ba885d471e00f486022ee10ef
hgatayaandf <121156667+hgatayaandf@users.noreply.github.com>
2025-08-10 19:04:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello from ECS Node.js App!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`App running on port ${port}`);
 });
+
